815a6458bf9c4848eea0fb5f7e89aea43a2f70a2
Jakub <staniojakub@gmail.com>
2019-02-04 17:35:14 +0100

    To Deploy

---
+++ public/build/precache-manifest.2ddf8916b5f941961c2afe24a2dd46d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c491f0a6087a1f7a2b2d",
+    "url": "/static/js/main.c491f0a6.chunk.js"
+  },
+  {
+    "revision": "76486e20ff4a05d7ff71",
+    "url": "/static/js/1.76486e20.chunk.js"
+  },
+  {
+    "revision": "c491f0a6087a1f7a2b2d",
+    "url": "/static/css/main.24c189f8.chunk.css"
+  },
+  {
+    "revision": "d0abdcbbcd2f24a67975bbd17e70a108",
+    "url": "/index.html"
+  }
+];
